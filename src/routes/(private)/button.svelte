6ec220bce012ff4e471234503da2186e7f00a84c
ItsAAA <imcengineer03@gmail.com>
2024-10-25 22:33:45 +0330

    feat: add a local button component

---
+++ src/routes/(private)/button.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { cn } from '$lib';
+    import type { HTMLButtonAttributes } from 'svelte/elements';
+	import { scale } from 'svelte/transition';
+    let { class: className, children, ...props }: HTMLButtonAttributes = $props();
+
+</script>
+
+<button
+	class={cn(
+		'text-foreground-100 size-7 text-center rounded-lg transition active:scale-95',
+		className,
+	)}
+	transition:scale
+	{...props}
+>
+	{#if children}
+		{@render children()}
+	{/if}
+</button>
