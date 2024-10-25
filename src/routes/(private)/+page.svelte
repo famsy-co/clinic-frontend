5e95aca80dd759ebde24f998e44f327a9ff500b4
ItsAAA <imcengineer03@gmail.com>
2024-10-25 22:34:50 +0330

    feat: use components in home page

--- src/routes/(private)/+page.svelte
+++ src/routes/(private)/+page.svelte
@@ -1,16 +1,20 @@
-<script>
+<script lang="ts">
 	import { m } from '$lib';
 	import { useAuth } from '$lib/providers/auth-guard.svelte';
+	import { LogOut } from 'svelte-feathers';
+	import Information from './information.svelte';
+	import DoctorsTable from './doctors-table.svelte';
 	const auth = useAuth();
 </script>
 
-<h1 class="flex bg-red-100 text-black">Welcome to SvelteKit</h1>
-<p>
-	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-	documentation
-</p>
-{m.hello_world({
-	name: auth.user?.name ?? '',
-})}
 
-<button onclick={auth.logout}>logout</button>
+<!-- HTML -->
+<header class="flex justify-between bg-dark-main-100 h-16 text-foreground-100 text-2xl items-center px-5">
+    <p>کلینیک رویال</p>
+    <button onclick={auth.logout}>
+    	<LogOut class="size-5"/>
+    </button>
+</header>
+
+<Information />
+<DoctorsTable />
